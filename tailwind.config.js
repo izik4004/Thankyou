e3bd46a625ff86829588c0f4126ccef4ae346151
unknown <izik4004@gmail.com>
2021-12-12 23:52:50 +0100

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'green-light': '#E7F3D2',  
+        'green-dark': '#85C11F',
+        'green-darker': '#436110'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
